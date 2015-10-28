ae62637885c36ba89d29e0e6dc9e2ca116226e42
Jay Versluis <jay@versluis.com>
2015-10-28 10:15:48 -0400

    Initial Commit

---
+++ Altitude Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Altitude Test
+//
+//  Created by Jay Versluis on 28/10/2015.
+//  Copyright © 2015 Pinkstone Pictures LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Altitude Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Altitude Test
+//
+//  Created by Jay Versluis on 28/10/2015.
+//  Copyright © 2015 Pinkstone Pictures LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
